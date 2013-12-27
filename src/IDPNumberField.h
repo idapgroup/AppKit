22609d5ac744cc39e663a8199eb231310ffa9073
Artem Chabanniy <artem.chabanniy@gmail.com>
2013-12-27 16:02:53 +0200

    Add IDPNumberFiled.

---
+++ src/IDPNumberField.h
@@ -0,0 +1,13 @@
+//
+//  IDPNumberField.h
+//  TimeTo
+//
+//  Created by Artem Chabanniy on 27/12/2013.
+//  Copyright (c) 2013 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IDPNumberField : NSTextField
+
+@end
