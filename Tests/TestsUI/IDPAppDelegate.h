b5d191c734e8273fb0734b82f3567ed87e528de3
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2015-01-09 19:12:38 +0200

    src -> renamed to Module Tests -> initial project commit

---
+++ Tests/TestsUI/IDPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDPAppDelegate.h
+//  TestsUI
+//
+//  Created by Oleksa Korin on 9/1/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IDPAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Tests/TestsUI/IDPAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  IDPAppDelegate.m
+//  TestsUI
+//
+//  Created by Oleksa Korin on 9/1/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import "IDPAppDelegate.h"
+
+@implementation IDPAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
